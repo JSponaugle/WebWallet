032d41b6dbe65cd9efbaa967350f276f6859b9ef
Yoshi <jason@htmllabs.com>
2017-05-04 15:50:00 -0400

    Display Transactions

---
+++ wallet/resources/views/layout/design1/transactions.blade.php
@@ -0,0 +1,29 @@
+<div class="row contentrow" id="transactions" style="background-color: darkgrey">
+    <div class="col-xs-12">
+        <table id="transactionsList" class="display table table-hover">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Date</th>
+                <th>Type</th>
+                <th>Address</th>
+                <th>Amount</th>
+            </tr>
+            </thead>
+            @foreach ($listtransactions as $each)
+                <tr>
+                    <th></th>
+                    <th>{!! date('m/d/Y H:i',$each['time']) !!}</th>
+                    <th>@if ($each['category'] == "receive") Received with @elseif($each['category'] == "send") Sent to @endif</th>
+                    <th>({!! $each['address'] !!})</th>
+                    <th style="color: @if ($each['category'] == "receive") green @elseif($each['category'] == "send") red @endif">@if ($each['category'] == "receive") + @elseif($each['category'] == "send") - @endif{!! abs($each['amount']) !!}</th>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
+<script>
+    $(document).ready(function () {
+        $('#transactionsList').DataTable();
+    });
+</script>
