b02a4df8d91e45e6af9198c83b0c31c88dd4edf2
Yoshi <jason@htmllabs.com>
2017-05-04 15:50:08 -0400

    Display Dashboard

---
+++ wallet/resources/views/layout/design1/dashboard.blade.php
@@ -0,0 +1,43 @@
+<div class="row contentrow" id="dashboard" style="background-color: darkgrey">
+    <div class="col-xs-5">
+        <div class="row">
+            <div class="col-xs-12" style="color: #000000; font-weight: 800;">Balances</div>
+        </div>
+        <div class="row">
+            <div class="col-xs-3" style="color: #000000">Available</div>
+            <div class="col-xs-9" style="color: #000000; font-weight: 800;">{!! $getinfo['balance'] !!} ION</div>
+        </div>
+        <div class="row">
+            <div class="col-xs-3" style="color: #000000">Stake</div>
+            <div class="col-xs-9" style="color: #000000; font-weight: 800;">{!! $getinfo['stake'] !!} ION</div>
+        </div>
+        <div class="row">
+            <div class="col-xs-3" style="color: #000000">Pending</div>
+            <div class="col-xs-9" style="color: #000000; font-weight: 800;">{!! $getinfo['newmint'] !!} ION</div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-3" style="color: #000000">Total</div>
+            <div class="col-xs-9" style="color: #000000; font-weight: 800;">{!! $total = $getinfo['balance'] + $getinfo['stake'] + $getinfo['newmint'] !!} ION</div>
+        </div>
+    </div>
+    <div class="col-xs-7">
+        <div class="row">
+            <div class="col-xs-12" style="color: #000000"> Recent Transactions</div>
+        </div>
+        @foreach ($listtransactions as $oneTrans)
+            <div class="row">
+                <div class="col-xs-2" style="color: #000000"><i class="fa @if ($oneTrans['category'] == "receive")fa-sign-in @elseif($oneTrans['category'] == "send") fa-sign-out @endif fa-4x" aria-hidden="true"></i></div>
+                <div class="col-xs-7" style="color: #000000">
+                    <div class="row">
+                        {!! date('m/d/Y H:i',$oneTrans['time']) !!}
+                    </div>
+                    <div class="row">
+                        ({!! $oneTrans['address'] !!})
+                    </div>
+                </div>
+                <div class="col-xs-3" style="color: @if ($oneTrans['category'] == "receive") #000000 @elseif($oneTrans['category'] == "send") red @endif">@if ($oneTrans['category'] == "receive") + @elseif($oneTrans['category'] == "send") - @endif{!! abs($oneTrans['amount']) !!} ION</div>
+            </div>
+        @endforeach
+    </div>
+</div>
