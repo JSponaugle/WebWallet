e29997f44408ca5c93cd8055a959a4442ee2cd41
Yoshi <jason@htmllabs.com>
2017-05-04 15:49:26 -0400

    Processing CSRF for Laravel Security

--- wallet/resources/views/wallet.blade.php
+++ wallet/resources/views/wallet.blade.php
@@ -1,4 +1,5 @@
 @include('layout.header')
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <body>
 <div class="container-fluid">
     <div class="row" style="margin-top:5%;"></div>
@@ -7,4 +8,11 @@
 </div>
 </body>
 @include('layout.footer')
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
 </html>
