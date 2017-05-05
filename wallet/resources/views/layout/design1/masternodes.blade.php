21a31988ec6777a3b95afec82bc35dd1a176fd74
Yoshi <jason@htmllabs.com>
2017-05-05 10:35:03 -0400

    Created MasterNodeList

--- wallet/resources/views/layout/design1/masternodes.blade.php
+++ wallet/resources/views/layout/design1/masternodes.blade.php
@@ -1,7 +1,40 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yoshi
- * Date: 5/4/2017
- * Time: 3:18 PM
- */
+<div class="row contentrow" id="addresses" style="background-color: darkgrey">
+    <div class="col-xs-12">
+        <ul class="nav nav-tabs">
+            <li class="network active" onclick=""><a>{!! env('WALLET0_TYPE') !!} Network</a></li>
+            <li class="mynodes" onclick=""><a>My Master Nodes</a></li>
+        </ul>
+        <div class="ionNetwork">
+        <table id="receiveList" class="display table table-hover">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Address</th>
+                <th>Active</th>
+                <th>Active (secs)</th>
+                <th>Last Seen</th>
+                <th>Pubkey</th>
+            </tr>
+            </thead>
+            @if (count($masterNodeList) > 0)
+                <script>console.log({!! json_encode($masterNodeList) !!})</script>
+                @foreach ($masterNodeList as $key=>$eachMN)
+                    <tr>
+                        <td>{!! ++$key !!}</td>
+                        <td>{!! $eachMN['address'] !!}</td>
+                        <td>{!! $eachMN['active'] !!}</td>
+                        <td>{!! $eachMN['activesec'] !!}</td>
+                        <td>{!! $eachMN['lastseen'] !!}</td>
+                        <td>{!! $eachMN['pubkey'] !!}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </table>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function () {
+        $('#receiveList').DataTable();
+    });
+</script>
