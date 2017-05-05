9d5529579006eb8a4dc05f201197cc55bd3bcff4
Yoshi <jason@htmllabs.com>
2017-05-05 09:35:42 -0400

    Messing around with Concept Design

--- wallet/resources/views/wallet.blade.php
+++ wallet/resources/views/wallet.blade.php
@@ -5,6 +5,7 @@
     <div class="row" style="margin-top:5%;"></div>
     @include('layout.design1.wallet')
     <div class="row"></div>
+    <div class="row"></div>
 </div>
 </body>
 @include('layout.footer')
